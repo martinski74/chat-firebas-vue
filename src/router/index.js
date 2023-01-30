a84e563115f1ac8317c05087cad058631263c445
martinski74 <martin.dobr@abv.bg>
2023-01-31 00:31:22 +0200

    split App to 2 components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Login from "./../components/Login.vue";
+import HomeView from "./../components/HomeView.vue";
 
-const routes = [];
+const routes = [
+  { path: "/login", component: Login },
+  { path: "/home", component: HomeView },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    username: "",
+    messages: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    SET_USER(state, user) {
+      state.username = user;
+    },
+    GET_USER(state) {},
   },
   actions: {
+    setUser({ commit }, user) {
+      commit("SET_USER", user);
+    },
   },
-  modules: {
-  }
-})
+});
